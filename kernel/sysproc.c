e396c0a6bcbcca03d9935d1d3d8317a10b0c0f74
追猪八级 <airali419@qq.com>
2025-09-03 16:12:55 +0800

    Finish traps lab

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -57,7 +57,7 @@ sys_sleep(void)
 {
   int n;
   uint ticks0;
-
+  backtrace();
   if(argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
@@ -95,3 +95,36 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void) {
+    int interval;
+    uint64 handler;
+    struct proc *p;
+    // 要求时间间隔非负
+    if (argint(0, &interval) < 0 || argaddr(1, &handler) < 0 || interval < 0) {
+        return -1;
+    }
+    // lab4-3
+    p = myproc();
+    p->interval = interval;
+    p->handler = handler;
+    p->passedticks = 0;    // 重置过去时钟数
+
+    return 0;
+}
+
+
+// lab4-3
+uint64 sys_sigreturn(void) {
+    struct proc* p = myproc();
+    // trapframecopy must have the copy of trapframe
+    if(p->trapframecopy != p->trapframe + 512) {
+        return -1;
+    }
+    memmove(p->trapframe, p->trapframecopy, sizeof(struct trapframe));   // restore the trapframe
+    p->passedticks = 0;     // prevent re-entrant
+    p->trapframecopy = 0;    // 置零
+    return p->trapframe->a0;	// 返回a0,避免被返回值覆盖
+}
+
+
